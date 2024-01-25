a5162f65c1ee2a2264afd43c8ea1b49cbda60de1
Andrew Bastien <volundmush@gmail.com>
2024-01-25 11:33:16 -0500

    Python Parser is working again!

--- bartholos/portal/events.py
+++ bartholos/portal/events.py
@@ -1,3 +1,14 @@
+from rich.highlighter import ReprHighlighter
+from rich.text import Text as RealRichText
+
+
+async def RichText(session, event: str, data):
+    if (txt := data.pop("text", None)) is not None:
+        text = RealRichText(txt, **data)
+        rendered = session.print(text)
+        await session.send_game_text(rendered)
+
+
 async def Text(session, event: str, data):
     if (txt := data.get("data", None)) is not None:
         await session.send_game_text(txt)
@@ -27,3 +38,38 @@ async def Table(session, event: str, data):
 
     rendered = session.print(table)
     await session.send_game_text(rendered)
+
+
+async def ReprEcho(session, event: str, data):
+    code = data.get("code", "")
+    highlighted = session.console.render_str(
+        code,
+        markup=False,
+        highlight=True,
+        highlighter=ReprHighlighter(),
+    )
+
+    rendered = session.print(highlighted)
+
+    rendered = f">>> {rendered}"
+
+    await session.send_text(rendered)
+
+
+async def ReprResult(session, event: str, data):
+    code = data.get("code", "")
+    highlighted = session.console.render_str(
+        code,
+        markup=False,
+        highlight=True,
+        highlighter=ReprHighlighter(),
+    )
+
+    rendered = session.print(highlighted)
+
+    rendered = f"<<< {rendered}"
+
+    if runtime := data.get("runtime", None):
+        rendered += f"\r\n ( runtime ~ {(runtime * 1000):.4f}ms )"
+
+    await session.send_text(rendered)
--- bartholos/server/repl.py
+++ bartholos/server/repl.py
@@ -3,41 +3,10 @@ import sys
 import time
 import bartholos
 import traceback
-from dataclasses import dataclass, field
-from rich.highlighter import ReprHighlighter
-from rich.text import Text
 
 from aioconsole.console import AsynchronousConsole
 
 from .game_session import SessionParser
-
-
-@dataclass
-class PythonRepresentation:
-    data: str
-    runtime: float | None = None
-    prefix: str | None = None
-
-    async def at_portal_receive(self, sess):
-        rendered = sess.console.render_str(
-            self.data,
-            markup=False,
-            highlight=True,
-            highlighter=ReprHighlighter(),
-        )
-
-        sess.console.print(rendered)
-        rendered = sess.console.export_text(clear=True, styles=True).rstrip()
-
-        if self.prefix:
-            out = f"{self.prefix} {rendered}"
-        else:
-            out = rendered
-
-        if self.runtime:
-            out += f"\r\n ( runtime ~ {(self.runtime * 1000):.4f}ms )"
-
-        await sess.send_text(out)
 
 
 class PythonParser(SessionParser, AsynchronousConsole):
@@ -78,7 +47,7 @@ class PythonParser(SessionParser, AsynchronousConsole):
         self.py_buffer = ""
 
     def get_locals(self) -> dict[str, "any"]:
-        return {"self": self.session, "mudforge": mudforge, "bartholos": bartholos}
+        return {"self": self.session, "bartholos": bartholos}
 
     def write(self, string):
         """Don't send to stderr, send to self.caller."""
@@ -99,24 +68,24 @@ class PythonParser(SessionParser, AsynchronousConsole):
         return result
 
     async def on_start(self):
-        out = Sendable()
-        out.add_renderable(
-            Text("Python REPL Console for Bartholos", style="bold green")
+        self.session.send_event(
+            "RichText",
+            {"text": "Python REPL Console for Bartholos", "style": "bold green"},
         )
-        out.add_renderable(
-            Text(f"Python {sys.version} on {sys.platform}", style="bold green")
+        self.session.send_event(
+            "RichText",
+            {"text": f"Python {sys.version} on {sys.platform}", "style": "bold green"},
         )
-        out.add_renderable(Text(f"( use quit() to exit )", style="bold green"))
-        msg = ServerSendables()
-        msg.add_sendable(out)
-        await self.session.outgoing_queue.put(msg)
+        self.session.send_event(
+            "RichText",
+            {"text": f"( use quit() to exit )", "style": "bold green"},
+        )
 
     async def on_close(self):
-        out = Sendable()
-        out.add_renderable(Text("Python Console closed.", style="bold green"))
-        msg = ServerSendables()
-        msg.add_sendable(out)
-        await self.session.outgoing_queue.put(msg)
+        self.session.send_event(
+            "RichText",
+            {"text": f"Python Console closed.", "style": "bold green"},
+        )
 
     async def parse(self, text: str):
         if text in ("exit", "exit()", "quit", "quit()", "close", "close()"):
@@ -126,13 +95,14 @@ class PythonParser(SessionParser, AsynchronousConsole):
             return
 
         self.py_buffer = ""
-        out = PythonRepresentation(text, prefix=">>>")
-        await self.session.outgoing_queue.put(out)
+        self.session.send_event("ReprEcho", {"code": text})
+
         t0 = time.time()
         results = await self.push(text)
         t1 = time.time()
-        out = PythonRepresentation(self.py_buffer.rstrip(), runtime=t1 - t0)
-        await self.session.outgoing_queue.put(out)
+        self.session.send_event(
+            "ReprResult", {"code": self.py_buffer.rstrip(), "runtime": t1 - t0}
+        )
 
     def showtraceback(self):
         """Display the exception that just occurred.
